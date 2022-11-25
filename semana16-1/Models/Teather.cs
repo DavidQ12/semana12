cc9bd15a8426670f61268bec0a39f2703efc2978
Alumnos <Alumnos@LABC2PC10>
2022-11-25 12:01:46 -0600

    Agregar archivos de proyecto.

---
+++ semana16-1/Models/Teather.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace semana16_1.Models
+{
+    public class Teather
+    {
+        public int Id { get; set;  }
+
+        public int Name { get; set;  }
+
+        public int Location { get; set;  }
+
+        public int Cinemas { get; set;  }
+
+        public decimal  Price { get; set;  }
+
+
+    }
+}
